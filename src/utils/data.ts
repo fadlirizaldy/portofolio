7321ffe1299b1f4deff3615adfd425064ea9eb45
fadlirizaldy <fadli.rizaldi12@gmail.com>
2024-02-04 17:52:00 +0700

    first commit

---
+++ src/utils/data.ts
@@ -0,0 +1,9 @@
+export const DataProfile = {
+  name: "Fadli Rizaldy",
+  passion: "Software Engineer",
+  about: `A Bachelor of Physics at Diponegoro University. Have keen interest to explore in web development, data analysis,
+    machine learning, and IoT in real work environment. Have a strong sense of responsibility and caring personality. A
+    tenacious person that really adaptable to work in team with big will to learn.`,
+  age: 23,
+  address: "Gading Serpong, Tangerang",
+};
