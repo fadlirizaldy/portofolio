ff0bd11ba82989cd62f7b7238a2d4f9b7a859727
fadlirizaldy <fadli.rizaldi12@gmail.com>
2024-02-05 07:20:46 +0700

    feat: add navbar section

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+import { Roboto } from "next/font/google";
+import { useRouter } from "next/router";
+
+const roboto = Roboto({
+  weight: "700",
+  style: "italic",
+  subsets: ["latin"],
+});
+
+const Navbar = () => {
+  const router = useRouter();
+  return (
+    <div className="max-w-[1200px] w-[90%] mx-auto py-3 flex justify-between">
+      <div className="flex items-center gap-1">
+        <h1 className={`${roboto.className} tracking-wider italic text-2xl`}>Fadli</h1>
+        <div className="w-3 h-3 bg-primaryBtn rounded-full"></div>
+      </div>
+      <div className="flex items-center gap-4 font-medium">
+        <Link href={"/"} className={`${router.asPath === "/" && "text-primaryBtn"} hover:underline`}>
+          About
+        </Link>
+        <Link href={"/works"} className={`${router.asPath === "/works" && "text-primaryBtn"} hover:underline`}>
+          Works
+        </Link>
+        <Link href={"/"} className={`${router.asPath === "/blog" && "text-primaryBtn"} hover:underline`}>
+          Blog
+        </Link>
+        <Link href={"/"} className={`${router.asPath === "/contact" && "text-primaryBtn"} hover:underline`}>
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
