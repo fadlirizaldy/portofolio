2c80b1d458d4ded5baa91aa2e44a31dcc3cc02c0
fadlirizaldy <fadli.rizaldi12@gmail.com>
2024-02-05 07:21:09 +0700

    feat: add section in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import MainLayout from "@/components/MainLayout";
+import { DataProfile } from "@/utils/data";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <h2>Hello</h2>
-    </div>
+    <MainLayout>
+      <div className="flex items-center justify-center py-12 gap-20 max-w-[1200px] w-[90%] mx-auto">
+        <section>
+          <h2>Hi, I am {DataProfile.name}</h2>
+          <p>Your Software Engineer</p>
+        </section>
+        <img src="/fadli-photo.jpg" alt="" className="w-48 h-48 rounded-full object-cover" />
+      </div>
+      <div className="bg-primary bg-opacity-20 h-56">
+        <div className="h-full flex items-center gap-4 max-w-[1200px] w-[90%] mx-auto">
+          <div className="w-40 h-40 bg-white rounded-xl">
+            <h2>Hello</h2>
+          </div>
+          <div className="w-40 h-40 bg-white rounded-xl">
+            <h2>Hello 2</h2>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
   );
 }
---
+++ src/pages/works/index.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/components/MainLayout";
+import React from "react";
+
+const WorksPage = () => {
+  return (
+    <MainLayout>
+      <h2>Hello Work</h2>
+    </MainLayout>
+  );
+};
+
+export default WorksPage;
