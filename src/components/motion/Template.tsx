656cd9362090aa02ffd1af2881f9e25b3be460e9
Fadli Rizaldy <85619226+fadlirizaldy@users.noreply.github.com>
2024-07-07 12:28:48 +0700

    fix: type show bottom 2

--- src/components/motion/Template.tsx
+++ src/components/motion/Template.tsx
@@ -6,7 +6,7 @@ const variants = {
   enter: { opacity: 1, y: 0, x: 0 },
 };
 
-export default function ShowFromBottom({ children, className, onClick, key }: { children: React.ReactNode, className?:string,onClick?:()=>void(), key:any }) {
+export default function ShowFromBottom({ children, className, onClick, key }: { children: React.ReactNode, className?:string,onClick?:()=>void, key:any }) {
   return (
     <motion.div key={key} variants={variants} initial="hidden" whileInView="enter" transition={{ type: "linear", duration: 0.8, staggerChildren: 0.5 }} className={className} onClick={onClick}>
       {children}
