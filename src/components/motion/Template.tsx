70adbefeb519936b5a7d81a9d09eb3739424747c
Fadli Rizaldy <85619226+fadlirizaldy@users.noreply.github.com>
2024-07-07 13:20:33 +0700

    fix: remove variants

--- src/components/motion/Template.tsx
+++ src/components/motion/Template.tsx
@@ -2,26 +2,8 @@
 import { motion } from "framer-motion";
 
 const variants = {
-  enter: {
-    x: 0,
-    y: 0,
-    opacity: 1,
-    transition: {
-      when: 'beforeChildren',
-      staggerChildren: 0.2,
-      delay: 0.3,
-    },
-  },
-  hidden: {
-    x: 0,
-    y:30,
-    opacity: 0,
-    transition: {
-      when: 'afterChildren',
-    },
-  },
-  // hidden: { opacity: 0, y: 30, x: 0 },
-  // enter: { opacity: 1, y: 0, x: 0 },
+  hidden: { opacity: 0, y: 30, x: 0 },
+  enter: { opacity: 1, y: 0, x: 0 },
 };
 
 export default function ShowFromBottom({ children, className }: { children: React.ReactNode, className?:string }) {
