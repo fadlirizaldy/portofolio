f0a4cd63c11a0a7af3d66ba9aacb5fb1fd287e1a
Fadli Rizaldy <fadly.rizaldy@ai.astra.co.id>
2025-07-03 15:59:17 +0700

    feat: add tech stack in works detail

--- src/pages/works/[id]/index.tsx
+++ src/pages/works/[id]/index.tsx
@@ -24,16 +24,27 @@ const WorkDetail = () => {
           className="absolute text-black top-4 flex items-center cursor-pointer"
           onClick={() => router.push("/works")}
         >
-          <Icon icon="tabler:arrow-left" className="text-xl h-full dark:text-white" />
+          <Icon
+            icon="tabler:arrow-left"
+            className="text-xl h-full dark:text-white"
+          />
         </div>
         <div className="flex flex-col md:flex-row gap-10 mt-7 p-3 border border-slate-300 rounded-lg">
           <div className="w-full md:w-80 md:max-h-56">
-            <img src={data?.image} alt="project-img" className="w-full h-full object-cover rounded-lg" />
+            <img
+              src={data?.image}
+              alt="project-img"
+              className="w-full h-full object-cover rounded-lg"
+            />
           </div>
           <div className="w-full md:w-[70%]">
             <div className="px-4 sm:px-0">
-              <h3 className="text-xl font-semibold leading-7 text-gray-900 dark:text-primaryBtn">{data?.title}</h3>
-              <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500 dark:text-gray-200">Project details</p>
+              <h3 className="text-xl font-semibold leading-7 text-gray-900 dark:text-primaryBtn">
+                {data?.title}
+              </h3>
+              <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500 dark:text-gray-200">
+                Project details
+              </p>
               <div className="flex items-center gap-2 mt-2">
                 <Link
                   href={data?.links?.live ?? ""}
@@ -42,7 +53,11 @@ const WorkDetail = () => {
                   className="tooltip"
                   data-tip="Live"
                 >
-                  <Icon icon="fluent:live-20-filled" className="dark:text-slate-200 text-slate-600" width={24} />
+                  <Icon
+                    icon="fluent:live-20-filled"
+                    className="dark:text-slate-200 text-slate-600"
+                    width={24}
+                  />
                 </Link>
                 <Link
                   href={data?.links?.github ?? ""}
@@ -51,28 +66,46 @@ const WorkDetail = () => {
                   className="tooltip"
                   data-tip="Github"
                 >
-                  <Icon icon="mdi:github" className="dark:text-slate-200 text-slate-600" width={24} />
+                  <Icon
+                    icon="mdi:github"
+                    className="dark:text-slate-200 text-slate-600"
+                    width={24}
+                  />
                 </Link>
               </div>
             </div>
             <div className="mt-6 border-t border-gray-100">
               <dl className="divide-y divide-gray-100 ">
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-primaryBtn">Type</dt>
+                  <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-primaryBtn">
+                    Type
+                  </dt>
                   <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 dark:text-white">
                     {data?.type}
                   </dd>
                 </div>
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-primaryBtn">Year</dt>
+                  <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-primaryBtn">
+                    Year
+                  </dt>
                   <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 dark:text-white">
                     {data?.year}
                   </dd>
                 </div>
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                  <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-primaryBtn">Description</dt>
+                  <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-primaryBtn">
+                    Description
+                  </dt>
                   <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 dark:text-white">
                     {data?.description}
+                  </dd>
+                </div>
+                <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                  <dt className="text-sm font-medium leading-6 text-gray-900 dark:text-primaryBtn">
+                    Stack
+                  </dt>
+                  <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 dark:text-white">
+                    {data?.tech_stack.join(", ")}
                   </dd>
                 </div>
               </dl>
